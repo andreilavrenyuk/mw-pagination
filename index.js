38d81ec6f27c8fabedb9f1f19d1ab98371b9b4cf
andrei.lavrenyuk <andrei.lavrenyuk@gmail.com>
2017-01-29 15:08:57 +0200

    feat: init project

---
+++ index.js
@@ -0,0 +1,19 @@
+
+function Pagination(params) {
+  const opts = {
+    limit: 10
+  };
+
+  Object.assign(opts, params || {});
+
+  this.init = (ctx, next) => {
+    const limit = Number(ctx.request.query.limit) || opts.limit;
+    const page  = (Number(ctx.request.page) || 1) - 1;
+    const skip  = page * limit;
+    ctx._pag = { limit, page, skip };
+    next();
+  };
+}
+
+
+module.exports = Pagination;
